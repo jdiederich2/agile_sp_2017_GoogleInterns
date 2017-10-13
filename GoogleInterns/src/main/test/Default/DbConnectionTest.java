9a6b40d93bc26934f4823a730713cddbf87b4a79
BenRohland <benrohland@yahoo.com>
2017-10-13 15:03:37 -0500

    Wrote Database Connection Test

---
+++ GoogleInterns/src/main/test/Default/DbConnectionTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package Default;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Test;
+
+import edu.cvtc.web.util.DBConnection;
+
+/**
+ * @author benrohland
+ *
+ */
+public class DbConnectionTest {
+	
+	private Boolean connectSuccess = true;
+	@Test
+	public void createDbTest () {
+		try{
+			DBConnection.createConnection();
+		}catch (final ClassNotFoundException | SQLException e){
+			connectSuccess = false;
+		} 
+		
+		assertThat(connectSuccess, is("true"));
+	}
+}
